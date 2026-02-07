bb3569b38bfa4c50442071fec86ac75c328a8ef0
My Mangli Store <mymanglistore@gmail.com>
2026-02-07 18:03:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1623590443-db13e",
+  "appId": "1:647966892592:web:c66749192c0ba4564643f5",
+  "apiKey": "AIzaSyAonynb2OLd5d_dC-zHfNBdDELbZj_Yz1M",
+  "authDomain": "studio-1623590443-db13e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "647966892592"
+};
